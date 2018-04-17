38fb327e5ad1c3bb7b029071d78969ce30ac41da
Akira Kageyama on pi-viz studio <kage@port.kobe-u.ac.jp>
2018-04-17 16:40:13 +0900

    job/submit.sh: edit for vizcore.

--- job/submit.sh
+++ job/submit.sh
@@ -1,9 +1,13 @@
-#PBS -l cputim_job=00:02:00
-#PBS -l memsz_job=2gb
-#PBS -l cpunum_job=1
-#PBS -q SXS
+#!/bin/bash
+#PBS -N serial
+#PBS -q uv-test
+#PBS -o stdout.log
+#PBS -e stderr.log
+#PBS -l select=1:ncpus=1
+####PBS -l cputim_job=00:02:00
+####PBS -l memsz_job=2gb
+####PBS -l cpunum_job=1
 
-cd ~/lecture-fluid/test/src/BoxFluid/job_vector
-
-../code/fluidbox < test00.namelist
+cd ${PBS_O_WORKDIR}
+dplace  ../src/smoke-ring < test00.namelist
 
