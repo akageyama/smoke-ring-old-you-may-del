70c82232e616a0ab16e562d615efb6d3423fd977
Akira Kageyama on Oakforest-PACS <kage@port.kobe-u.ac.jp>
2018-05-16 12:02:16 +0900

    minor format changes.

--- src/vis_interface.cpp
+++ src/vis_interface.cpp
@@ -8,7 +8,11 @@
 #include <KVS.osmesa/Lib/Screen.h>
 #include <kvs/String>
 
-extern "C" void Isosurface( float values[], int size, int dimx, int dimy, int dimz ,int visstep, float isolevel)
+
+//-----------------------------------------------------------------------------
+extern "C"
+void Isosurface( float values[], int size, int dimx, int dimy, int dimz , int visstep, float isolevel)
+//-----------------------------------------------------------------------------
 {
   std::cout << "Vis_interface Start" << std::endl;
   kvs::StructuredVolumeObject* volume = new kvs::StructuredVolumeObject();
@@ -21,30 +25,32 @@ extern "C" void Isosurface( float values[], int size, int dimx, int dimy, int di
   volume->print(std::cout);
 
   //kvs::PolygonObject* object = new kvs::ExternalFaces( volume );
-  //const double i = ( volume->maxValue() + volume->minValue() ) * 0.5;
-  const double i = ( volume->maxValue() - volume->minValue() ) * isolevel + volume->minValue();
-  const kvs::PolygonObject::NormalType n = kvs::PolygonObject::VertexNormal;
-  const bool d = false;
-  const kvs::TransferFunction t( 256 );
-  kvs::PolygonObject* object = new kvs::Isosurface( volume , i, n, d, t );
+  const double val = ( volume->maxValue() - volume->minValue() ) * isolevel + volume->minValue();
+  const kvs::PolygonObject::NormalType ntype = kvs::PolygonObject::VertexNormal;
+  const bool duplication = false;
+  const kvs::TransferFunction tfunc( 256 );
+  kvs::PolygonObject* object = new kvs::Isosurface( volume , val, ntype, duplication, tfunc );
   delete volume;
   object->print(std::cout);
   const kvs::Mat3 R = kvs::Mat3::RotationX( 30 ) * kvs::Mat3::RotationY( 30 );
   object->multiplyXform( kvs::Xform::Rotation( R ) );
-  
+
   kvs::osmesa::Screen screen;
   screen.registerObject( object );
   screen.registerObject( object, new kvs::Bounds );
   screen.draw();
-  
+
   kvs::ColorImage image = screen.capture();
-  
-  std::string filename = "iso_image_"+ kvs::String::ToString( visstep) + ".bmp" ;
-  //std::string filename = "iso_image.bmp" ;
+
+  std::string filename = "iso_image_"+ kvs::String::ToString( visstep ) + ".bmp" ;
   image.write( filename );
 }
 
-extern "C" void SlicePlane( float values[], int size, int dimx, int dimy, int dimz, int visstep )
+
+//-----------------------------------------------------------------------------
+extern "C"
+void SlicePlane( float values[], int size, int dimx, int dimy, int dimz, int visstep )
+//-----------------------------------------------------------------------------
 {
   std::cout << "Vis_interface Start" << std::endl;
   kvs::StructuredVolumeObject* volume = new kvs::StructuredVolumeObject();
@@ -55,30 +61,33 @@ extern "C" void SlicePlane( float values[], int size, int dimx, int dimy, int di
   volume->updateMinMaxValues();
   volume->updateMinMaxCoords();
   volume->print(std::cout);
-  
+
   const kvs::Vector3f c( ( volume->maxObjectCoord() + volume->minObjectCoord() ) * 0.4f );
   const kvs::Vector3f p( c );
-  const kvs::Vector3f n( 1.0, 0.8, 1.0 );
-  const kvs::TransferFunction t( 256 );
+  const kvs::Vector3f n( 0.0, 0.0, 1.0 );
+  const kvs::TransferFunction tfunc( 256 );
 
-  kvs::PolygonObject* object = new kvs::SlicePlane( volume, p, n, t );
+  kvs::PolygonObject* object = new kvs::SlicePlane( volume, p, n, tfunc );
   delete volume;
   object->print(std::cout);
   const kvs::Mat3 R = kvs::Mat3::RotationX( 30 ) * kvs::Mat3::RotationY( 30 );
   object->multiplyXform( kvs::Xform::Rotation( R ) );
-  
+
   kvs::osmesa::Screen screen;
   screen.registerObject( object );
   screen.registerObject( object, new kvs::Bounds );
   screen.draw();
   kvs::ColorImage image = screen.capture();
 
-  std::string filename = "slice_image_"+ kvs::String::ToString( visstep) + ".bmp" ;
+  std::string filename = "slice_image_"+ kvs::String::ToString( visstep ) + ".bmp" ;
   image.write( filename );
-  //  image.write( "slice_image.bmp" );
 }
 
-extern "C" void RayCasting( float values[], int size, int dimx, int dimy, int dimz, int visstep )
+
+//-----------------------------------------------------------------------------
+extern "C"
+void RayCasting( float values[], int size, int dimx, int dimy, int dimz, int visstep )
+//-----------------------------------------------------------------------------
 {
   std::cout << "Vis_interface Start" << std::endl;
   kvs::StructuredVolumeObject* volume = new kvs::StructuredVolumeObject();
@@ -95,14 +104,13 @@ extern "C" void RayCasting( float values[], int size, int dimx, int dimy, int di
 
   const kvs::Mat3 R = kvs::Mat3::RotationX( 30 ) * kvs::Mat3::RotationY( 30 );
   volume->multiplyXform( kvs::Xform::Rotation( R ) );
-  
+
   kvs::osmesa::Screen screen;
   screen.registerObject( volume, renderer );
   screen.registerObject( volume, new kvs::Bounds );
   screen.draw();
   kvs::ColorImage image = screen.capture();
 
-  std::string filename = "raycast_image_"+ kvs::String::ToString( visstep) + ".bmp" ;
+  std::string filename = "raycast_image_"+ kvs::String::ToString( visstep ) + ".bmp" ;
   image.write( filename );
-  //  image.write( "raycast_image.bmp" );
 }
